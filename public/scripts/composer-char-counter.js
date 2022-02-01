7ff95c1a08f52e63ed3f9bdf505bfd011d77629e
ShayFae <ifrahnoor108@gmail.com>
2022-02-01 17:39:42 -0500

    Added character counter and tweet styles

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  let counter = $('.counter').html();
+  $('#tweet-text').keypress(function() {    
+   counter--
+   if(counter < 0) {
+    $('.counter').addClass('negative');
+  }
+   console.log(counter)
+   $('.counter').text(counter)
+ })
+});
