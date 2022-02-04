641785003fe69363e3751f7c417f5ffa59ee6950
ShayFae <ifrahnoor108@gmail.com>
2022-02-04 01:32:56 -0500

    Final before review

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,88 +1,87 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-$(document).ready(function() {
-  const renderTweets = function(tweets) {
-    // loops through tweets
-    for(const tweet of tweets) {
-      // console.log(tweet)
-    // calls createTweetElement for each tweet
-    let callTweet = createTweetElement(tweet)
-    // takes return value and appends it to the tweets container
-    $('#tweets-container').prepend(callTweet);
+(function ($) {
+  $(document).ready(function () {
+    //Appens the tweets by newest into an array
+    const renderTweets = function (tweets) {
+      // loops through tweets
+      for (const tweet of tweets) {
+        // console.log(tweet)
+        // calls createTweetElement for each tweet
+        let callTweet = createTweetElement(tweet)
+        // takes return value and appends it to the tweets container
+        $('#tweets-container').prepend(callTweet);
+      }
     }
-  }
 
-  //escape function given in compass
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
+    //escape function given in compass
+    const escape = function (str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
 
-  //Tweet template
-  const createTweetElement = function(callTweet) {
-  let $tweet = `
+    //Tweet template
+    const createTweetElement = function (callTweet) {
+      let $tweet = `
     <article>
       <div class="tweet-header">
-        <span><img class="avatarImg" src="${escape(callTweet.user.avatars)}"> ${escape(callTweet.user.name)}</span> <span>${escape(callTweet.user.handle)}</span>
+        <span><img class="avatarImg" src="${escape(callTweet.user.avatars)}"> ${escape(callTweet.user.name)}</span> <span class="tweet-handle">${escape(callTweet.user.handle)}</span>
       </div>
       <br>
       <p class="tweet-content">${escape(callTweet.content.text)}.</p>
-      <input type="text">
+      <input type="text" disabled>
       <footer>
         <span>${escape(timeago.format(callTweet.created_at))}</span> <span class="tweet-icons"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i> <i class="fas fa-heart"></i></span> 
       </footer>
     </article>
   `
-  return $tweet;
-  }
-
-  $('form').on('submit', function(event) {
-    // $('.counter').text(140)
-    //Put the textarea value take from it's attached class into a variable
-    let textareaVal = $('#tweet-text').val()
-    //conditions to check against the variable and return the error plus prevent it from posting
-    if(textareaVal.length === 0) {
-      $('.error').text('Text area is empty, please fill in and submit').slideDown();
-      return false
-    } else if(textareaVal.length > 140) {
-      $('.error').text("You've exceeded the maximum characters allowed, please fill in correctly and submit").slideDown();
-      return false;
-    } else if(textareaVal === null) {
-      $('.error').text("'Text area is null, please fill correctly and submit'").slideDown();
-      return false
-      //if none apply then we remove text within the div that has the error class
-    } else {
-      $('.error').empty();
+      return $tweet;
     }
+    //Listening for sumbit event on the form
+    $('form').on('submit', function (event) {
+      // $('.counter').text(140)
+      //Put the textarea value taken from it's attached class into a variable
+      let textareaVal = $('#tweet-text').val()
 
-    event.preventDefault();
-    const serForm = $('form').serialize();
+      // console.log('this is text', textareaVal[0])
+      if (textareaVal === ' ') {
+        console.log('yes')
+      }
+      //conditions to check against the variable and return the error plus prevent it from posting
+      if (textareaVal[0] === " " || textareaVal.length === 0) {
+        $('.error').text('Input is empty, please fill in correctly and submit⚠️').slideDown();
+        return false
+      } else if (textareaVal.length > 140) {
+        $('.error').text("You've exceeded the maximum characters allowed, please fill in correctly and submit⛔").slideDown();
+        return false;
+      } else if (textareaVal === null) {
+        $('.error').text('😔Text area is null 😔, please fill correctly and submit').slideDown();
+        return false
+        //if none apply then we remove text within the div that has the error class
+      } else {
+        $('.error').empty();
+      }
 
-    $.post('/tweets', serForm).then(function(data) {
-      //Keeps all added tweets in an object
-      renderTweets(data);
-      $.get('/tweets', function(data) {
-        console.log('this is load tweets', data.slice(-1));
-        //removes everything but the last(lastest) user information within the array
-        let slicedTweet = data.slice(-1);
-        renderTweets(slicedTweet);
-        // console.log(slicedTweet)
+      event.preventDefault();
+      const serForm = $('form').serialize();
+
+      $.post('/tweets', serForm).then(function (data) {
+        //Keeps all added tweets in an object
+        renderTweets(data);
+        $.get('/tweets', function (data) {
+          console.log('this is load tweets', data.slice(-1));
+          //removes everything but the last(lastest) user information within the array
+          let slicedTweet = data.slice(-1);
+          renderTweets(slicedTweet);
+          // console.log(slicedTweet)
+        })
       })
-    })
+    });
+    //gets the data and loads all the tweets
+    const loadTweets = function () {
+      $.get('/tweets', function (data) {
+        renderTweets(data);
+      })
+    }
+    loadTweets();
   });
-
-  const loadTweets = function() {
-    $.get('/tweets', function(data) {
-      // console.log('this is load tweets', data);
-      // tweetObj = data;
-      renderTweets(data);
-      // console.log('THIS IS WEIRD',tweetObj)
-    })
-  }
-  loadTweets();
-});
+})(jQuery)
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,25 @@
-$(document).ready(function() {
-  let counter = $('.counter').html();
-  $('#tweet-text').keypress(function() {    
-   counter--
-   if(counter < 0) {
-    $('.counter').addClass('negative');
-  }
-  //  console.log(counter)
-   $('.counter').text(counter)
- })
-});
+(function ($) {
+  $(document).ready(function () {
+    $('#tweet-text').on('input', function () {
+      //since we're withing the textarea this = to the value of it
+      let text = $(this);
+      //then minus the length of the text from the count
+      let count = 140 - text.val().length;
+
+      //A way to target the counter class is going through the tree 
+      //we go up from the child(textarea) and find the closest parent that  = form
+      let findParent = text.closest('form')
+      //then from that parent we go down through its children and find the matching class(.counter)
+      let findCounter = findParent.find('.counter')
+      // console.log(count)
+      //then we take that variable that contains the found class and replace the text within it with whatever value count has
+      findCounter.text(count)
+      if (count < 0) {
+        $('.counter').addClass('negative');
+        //Need else to remove class
+      } else {
+        $('.counter').removeClass('negative');
+      }
+    })
+  });
+})(jQuery);
